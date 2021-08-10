abfcb5bc90d6e59d155aa65f8bccd28b79cd1fb5
dbsqwerty123 <sean.seah0311@gmail.com>
2021-08-11 07:11:34 +0800

    DialogFlow Agent

--- app/src/main/java/com/dialogflow/chatbot/music_player.java
+++ app/src/main/java/com/dialogflow/chatbot/music_player.java
@@ -6,27 +6,22 @@ import android.os.IBinder;
 
 public class music_player extends Service {
     MediaPlayer player;
-
     @Override
     public IBinder onBind(Intent intent) {
         return null;
     }
-
     public void onCreate() {
         player = MediaPlayer.create(this, R.raw.music);
         player.setLooping(true); //set looping
     }
-
     public int onStartCommand(Intent intent, int flags, int startId) {
         player.start();
         return Service.START_NOT_STICKY;
     }
-
     public void onDestroy() {
         player.stop();
         player.release();
         stopSelf();
         super.onDestroy();
     }
-
 }
